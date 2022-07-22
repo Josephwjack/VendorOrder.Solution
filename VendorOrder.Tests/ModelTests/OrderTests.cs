41bf2e769b8182216c2816ac64dc53abb9183b3d
Josephwjack <joseph.w.jack@gmail.com>
2022-07-22 10:30:34 -0700

    order constructor test passed

--- VendorOrder.Tests/ModelTests/OrderTests.cs
+++ VendorOrder.Tests/ModelTests/OrderTests.cs
@@ -1,22 +1,22 @@
-// using System;
-// using Microsoft.VisualStudio.TestTools.UnitTesting;
-// using VendorOrder.Models;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendorOrder.Models;
 
-// namespace VendorOrder.Tests
-// {
-//   [TestClass]
-//   public class OrderTests : IDisposable
-//   {
-//     public void Dispose()
-//     {
-//       Vendor.ClearAll();
-//     }
+namespace VendorOrder.Tests
+{
+  [TestClass]
+  public class OrderTests 
+  {
+    // public void Dispose()
+    // {
+    //   Order.ClearAll();
+    // }
 
-//     [TestMethod]
-//     public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
-//     {
-      
-//       Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
-//     }
-//   }
-// }
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order("test");
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+  }
+}
--- VendorOrder.Tests/ModelTests/VendorTests.cs
+++ VendorOrder.Tests/ModelTests/VendorTests.cs
@@ -62,5 +62,18 @@ namespace VendorOrder.Tests
 
       Assert.AreEqual(1, result);
     }
+
+    [TestMethod]
+    public void Find_ReturnsCorrectVendor_Vendor()
+    {
+      string name01 = "bakeshop";
+      string name02 = "theunbakedshop";
+      Vendor newVendor1 = new Vendor(name01);
+      Vendor newVendor2 = new Vendor(name02);
+
+      Vendor result = Vendor.Find(2);
+
+      Assert.AreEqual(newVendor2, result);
+    }
   }
 }
