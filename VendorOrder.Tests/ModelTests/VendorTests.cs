ed4aa19f509342ed08b6a2a8d59d2229e7a0beff
Josephwjack <joseph.w.jack@gmail.com>
2022-07-22 10:11:58 -0700

    get all test failed, add static ClearAll and static GetAll

--- VendorOrder.Tests/ModelTests/VendorTests.cs
+++ VendorOrder.Tests/ModelTests/VendorTests.cs
@@ -1,16 +1,18 @@
 using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VendorOrder.Models;
+using System.Collections.Generic;
+
 
 namespace VendorOrder.Tests
 {
   [TestClass]
-  public class VendorTests 
+  public class VendorTests : IDisposable
   {
-    // public void Dispose()
-    // {
-    //   Vendor.ClearAll();
-    // }
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
 
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
@@ -27,6 +29,27 @@ namespace VendorOrder.Tests
 
       string result = newVendor.VendorName;
       Assert.AreEqual(vendorName, result);
+    }
+
+    [TestMethod]
+    public void SetVendorName_SetVendorName_String()
+    {
+      string vendorName = "test3";
+      Vendor newVendor = new Vendor(vendorName);
+
+      string updatedVendorName = "newtest";
+      newVendor.VendorName = updatedVendorName;
+      string result = newVendor.VendorName;
+      Assert.AreEqual(updatedVendorName, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_VendorList()
+    {
+      List<Vendor> newList = new List<Vendor> { };
+
+      List<Vendor> result = Vendor.GetAll();
+      CollectionAssert.AreEqual(newList, result);
     }
   }
 }
