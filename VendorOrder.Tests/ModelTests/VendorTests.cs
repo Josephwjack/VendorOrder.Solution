f905e525e4361d75f7dac797ddd85db7f403ce03
Josephwjack <joseph.w.jack@gmail.com>
2022-07-22 10:02:05 -0700

    test passed for returning vendor name of an instance

--- VendorOrder.Tests/ModelTests/VendorTests.cs
+++ VendorOrder.Tests/ModelTests/VendorTests.cs
@@ -18,5 +18,15 @@ namespace VendorOrder.Tests
       Vendor newVendor = new Vendor("test");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
+
+    [TestMethod]
+    public void GetVendorName_ReturnsVendorName_String()
+    {
+      string vendorName = "vendornametest";
+      Vendor newVendor = new Vendor(vendorName);
+
+      string result = newVendor.VendorName;
+      Assert.AreEqual(vendorName, result);
+    }
   }
 }
